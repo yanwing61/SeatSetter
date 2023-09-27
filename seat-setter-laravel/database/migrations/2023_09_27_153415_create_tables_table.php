9b1358433ae96764ed027c057c633612d07e9052
Yan Wing Pang <yanwing61@gmail.com>
2023-09-27 11:41:40 -0400

    add guests, tables, groups table

---
+++ seat-setter-laravel/database/migrations/2023_09_27_153415_create_tables_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id('table_id');
+            $table->integer('num_of_guest');
+            $table->foreignId('event_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+    }
+};
---
+++ seat-setter-laravel/database/migrations/2023_09_27_153922_create_guests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id('guest_id');
+            $table->string('guest_fname');
+            $table->string('guest_lname');
+            $table->string('guest_remarks');
+            $table->foreignId('group_id');
+            $table->foreignId('table_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guests');
+    }
+};
