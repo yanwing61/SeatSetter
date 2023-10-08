3788851f3b5e4de331b6ec62b7b5634c2fe57665
Yan Wing Pang <yanwing61@gmail.com>
2023-10-08 19:56:38 -0400

    add bootstrap and modify css

--- seat-setter-laravel/resources/views/layout/console.blade.php
+++ seat-setter-laravel/resources/views/layout/console.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SeatSetter</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
 
 </head>
 <body>
@@ -14,11 +14,25 @@
         <h1>SeatSetter Dashboard</h1>
 
         <?php if(Auth::check()): ?>
+            <nav> 
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a href="/console/dashboard">Dashboard</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a href="/">FAQ</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a href="/console/logout">Log Out</a>
+                    </li>
+                </ul>
+            </nav>
+
+        <p>
             Welcome! You're logged in as
-            <?= auth()->user()->name ?> | 
-            <a href="/console/logout">Log Out</a> | 
-            <a href="/console/dashboard">Dashboard</a> | 
-            <a href="/">HomePage</a>
+            <?= auth()->user()->name ?>
+        </p>
+
         <?php else: ?>
                 <a href="/">Return to My HomePage</a>
         <?php endif; ?>
@@ -35,5 +49,13 @@
 
     @yield ('content')
 
+    <footer id="footer">
+      <div id="footer-copyright">
+        &copy; Copyright Yan Wing Pang, 2023.
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="/app.js"></script>
 </body>
 </html>
