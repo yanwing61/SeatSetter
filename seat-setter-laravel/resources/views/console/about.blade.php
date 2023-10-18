9ae8ed5cf8cb2ae72855b9d51776ca6dc71a89d2
Yan Wing Pang <yanwing61@gmail.com>
2023-10-17 23:39:53 -0400

    modify css

--- seat-setter-laravel/resources/views/console/about.blade.php
+++ seat-setter-laravel/resources/views/console/about.blade.php
@@ -10,17 +10,17 @@
 
     <h3>Who is it for</h3>
     <ol>
-        <li>Event Organizers of All Scales: Whether you are orchestrating a large conference, planning an intimate dinner, or anything in between, SeatSetter is built to cater to your needs.</li>
-        <li>Wedding Planners: Struggling with seating charts for the big day? Let SeatSetter simplify the process, ensuring guests are seated just where you want them.</li>
-        <li>Corporate Event Managers: For seminars, conferences, and annual meetings, use SeatSetter to ensure every attendee has a spot and no table is overlooked.</li>
-        <li>Any Occasion, Any Size: From birthdays to anniversaries, fundraisers to gala dinners, SeatSetter's flexibility makes it the go-to choice for all your event planning needs.</li>
-    </ul>
+        <li><strong>Event Organizers of All Scales</strong>: Whether you are orchestrating a large conference, planning an intimate dinner, or anything in between, SeatSetter is built to cater to your needs.</li>
+        <li><strong>Wedding Planners</strong>: Struggling with seating charts for the big day? Let SeatSetter simplify the process, ensuring guests are seated just where you want them.</li>
+        <li><strong>Corporate Event Managers</strong>: For seminars, conferences, and annual meetings, use SeatSetter to ensure every attendee has a spot and no table is overlooked.</li>
+        <li><strong>Any Occasion, Any Size</strong>: From birthdays to anniversaries, fundraisers to gala dinners, SeatSetter's flexibility makes it the go-to choice for all your event planning needs.</li>
+    </ol>
 
     <h3>How does it work</h3>
     <ol>
-        <li>Guest & Table Management: Begin by adding guests and defining tables. Input essential details, group guests if required, and set table capacities effortlessly.</li>
-        <li>Drag, Drop, Done!: Navigate to the seating arrangement page, where you can simply drag guests and drop them onto the desired tables. The interface provides real-time visual feedback, making the process seamless.</li>
-        <li>Save & Share: Once you're satisfied with the arrangements, save your setup. The application offers diverse export options, allowing you to share the seating plan in formats like .csv, .pdf (list), and .pdf (image)</li>
+        <li><strong>Guest & Table Management</strong>: Begin by adding guests and defining tables. Input essential details, group guests if required, and set table capacities effortlessly.</li>
+        <li><strong>Drag, Drop, Done!</strong>: Navigate to the seating arrangement page, where you can simply drag guests and drop them onto the desired tables. The interface provides real-time visual feedback, making the process seamless.</li>
+        <li><strong>Save & Share</strong>: Once you're satisfied with the arrangements, save your setup. The application offers diverse export options, allowing you to share the seating plan in formats like .csv, .pdf (list), and .pdf (image)</li>
     </ol>
 
     <a href="/console/dashboard">Start Planning</a>
